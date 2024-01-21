3dbef49cf417439f70be9378f5c594ce905884c4
devrimcelik1 <149385023+devrimcelik1@users.noreply.github.com>
2024-01-21 13:09:23 +0300

    Add files via upload

---
+++ odev_4.java
@@ -0,0 +1,27 @@
+public class odev_4 {
+
+    public static void main(String[] args) {
+        ucgenOlustur1den5e();
+        ucgenOlustur5ten1e();
+    }
+
+    public static void ucgenOlustur1den5e() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void ucgenOlustur5ten1e() {
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
